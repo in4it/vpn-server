28ac06f5bfc0568e2d689ca7e905ab9740a74b74
Edward Viaene <ward@in4it.io>
2026-02-16 12:02:01 -0600

    extra input checks

--- webapp/src/Auth/OIDCAndSAMLRedirect.tsx
+++ webapp/src/Auth/OIDCAndSAMLRedirect.tsx
@@ -1,14 +1,20 @@
-import { useEffect } from "react";
-import { AppSettings } from "../Constants/Constants";
+import { useEffect, useState } from "react";
+import { AppSettings, UUID_REGEX } from "../Constants/Constants";
 import { useMatch } from "react-router-dom";
 
 export function OIDCAndSAMLRedirect() {
+  const [redirectError, setRedirectError] = useState<string>("")
   const loginMatch = useMatch("/login/:loginType/:loginID");
 
   useEffect(() => {
     if (loginMatch && loginMatch.params.loginType !== undefined && loginMatch.params.loginID !== undefined) {
-      window.location.href = AppSettings.url + '/authmethods/' + loginMatch.params.loginType + '/' + loginMatch.params.loginID + "/redirect"
+      if (!UUID_REGEX.test(loginMatch.params.loginID)) {
+        setRedirectError("Invalid OIDC method id (expected UUID)");
+        return;
+      }
+      window.location.href = AppSettings.url + '/authmethods/' + encodeURIComponent(loginMatch.params.loginType) + '/' + encodeURIComponent(loginMatch.params.loginID) + "/redirect"
     }
   }, []);
+  if (redirectError !== "") return <p>{redirectError}</p>
   return (<></>)
 }
--- webapp/src/Constants/Constants.tsx
+++ webapp/src/Constants/Constants.tsx
@@ -5,3 +5,6 @@ interface Settings {
 export var AppSettings: Settings = {
     url: "/api",
 }
+
+export const UUID_REGEX =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
